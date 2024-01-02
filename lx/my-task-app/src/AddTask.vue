7225c728e61d28957e7b8f7f3aace2a9910425fc
lxl <1602105334@qq.com>
2024-01-03 00:56:17 +0800

    提交day7

---
+++ lx/my-task-app/src/AddTask.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- AddTask 组件的模板 -->
+  <div>
+    <!-- 文本输入框，用于输入新任务 -->
+    <!-- 使用 v-model 双向绑定 newTask 数据属性 -->
+    <input v-model="newTask" @keyup.enter="submitTask">
+    <!-- 添加任务按钮，点击时调用 submitTask 方法 -->
+    <button @click="submitTask">Add Task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // newTask 用于存储输入框的值
+      newTask: ''
+    };
+  },
+  methods: {
+    // submitTask 方法用于处理新任务的提交
+    submitTask() {
+      // 触发 add-task 事件并将新任务作为参数传递
+      this.$emit('add-task', this.newTask);
+      // 清空输入框
+      this.newTask = '';
+    }
+  }
+}
+</script>
---
+++ lx/my-task-app/src/TaskList.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- TaskList 组件的模板 -->
+  <div>
+    <!-- 引入 AddTask 子组件，并监听 add-task 事件 -->
+    <!-- 当事件发生时，调用 addNewTask 方法 -->
+    <add-task @add-task="addNewTask"></add-task>
+
+    <!-- 循环渲染任务列表 -->
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// 导入 AddTask 子组件
+import AddTask from './AddTask.vue';
+
+export default {
+  components: {
+    // 在 components 对象中注册 AddTask 组件
+    AddTask
+  },
+  data() {
+    return {
+      // tasks 数组用于存储任务列表
+      tasks: ['Task 1', 'Task 2']
+    };
+  },
+  methods: {
+    // addNewTask 方法用于添加新任务到任务列表
+    addNewTask(newTask) {
+      this.tasks.push(newTask);
+    }
+  }
+}
+</script>
